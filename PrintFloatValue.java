f144b416d390da303f9f94c1fa27f40fbe0dd070
baradedaivshila <158499689+baradedaivshila@users.noreply.github.com>
2024-02-13 22:47:30 +0530

    Add files via upload

---
+++ PrintFloatValue.java
@@ -0,0 +1,15 @@
+class PrintFloatValue
+{
+    public static void main(String[]args)
+    {
+        float value1 = 10.5f;
+        System.out.println(value1);
+
+}
+    
+}
+/*
+ Compile time successful
+ Runtime successful 
+ 10.5
+ */
