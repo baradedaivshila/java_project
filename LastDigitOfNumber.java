838f8a08910edaf67865957275fecc48454895de
baradedaivshila <158499689+baradedaivshila@users.noreply.github.com>
2024-02-13 23:00:45 +0530

    Add files via upload

---
+++ LastDigitOfNumber.java
@@ -0,0 +1,16 @@
+class LastDigitOfNumber
+{
+    public static void main(String[]ldon)
+    {
+        int n=1258647;
+        int last_digit=n%10;
+        System.out.println(last_digit);
+
+}
+    
+}
+/*
+ compile time successful
+ runtime successful
+ 7
+ */
