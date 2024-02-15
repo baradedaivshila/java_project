ece7942da34872efcf43ee75931aaafbc2390b87
baradedaivshila <158499689+baradedaivshila@users.noreply.github.com>
2024-02-15 23:41:47 +0530

    Add files via upload

---
+++ vote.java
@@ -0,0 +1,37 @@
+class vote
+{
+    public static void main(String[]args)
+    {
+        int age1=58;
+        int age2=18;
+        int age3=12;
+        int age4=14;
+        int age5=17;
+        boolean n = age1>18;
+        boolean n2 = age2>18;
+        boolean n3 = age3>18;
+        boolean n4 = age4>18;
+        boolean n5 = age5>18;
+        
+        
+        
+        System.out.println("age1 are eligible for voting:"+n);
+        System.out.println("age2 are eligible for voting:"+n2);
+        System.out.println("age3 are eligible for voting:"+n3);
+        System.out.println("age4 are eligible for voting:"+n4);
+        System.out.println("age5 are eligible for voting:"+n5);
+        
+
+}
+    
+}
+/*
+ compile time successful
+ runtime successsful
+ age1 are eligible for voting:true
+ age2 are eligible for voting:false
+ age3 are eligible for voting:false
+ age4 are eligible for voting:false
+ age5 are eligible for voting:false
+ 
+ */
