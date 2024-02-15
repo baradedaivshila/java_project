050628e118a4d17db3d4e6ff804dec980cc27707
baradedaivshila <158499689+baradedaivshila@users.noreply.github.com>
2024-02-15 23:37:04 +0530

    Add files via upload

---
+++ Narrowingop.java
@@ -0,0 +1,18 @@
+public class Narrowingop {
+
+    public static void main(String[]args)
+    {
+        int a=130;
+        byte a1=(byte)a;
+        System.out.println(a1);//the range of byte is -128 to 127
+
+}
+    
+}
+/*
+ compile time success
+ runtime success
+ -126
+ */
+    
+
