6088452470ba3f7f2ca4749e95016a1a1da8037e
baradedaivshila <158499689+baradedaivshila@users.noreply.github.com>
2024-02-21 00:02:24 +0530

    Add files via upload

---
+++ Inputname.java
@@ -0,0 +1,24 @@
+
+    import java.util.Scanner;
+    public class Inputname {
+    public static void main(String[]args)
+    {
+        //object of scanner class
+        //To call inbuilt method for Scanner class
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter Your name");
+        String name = sc.next();
+        System.out.println("name");
+
+    }
+    
+}
+/*
+ compile time successful
+ runtime successful
+ enter Your name
+ Daivshila Barade
+ */
+
+    
+
